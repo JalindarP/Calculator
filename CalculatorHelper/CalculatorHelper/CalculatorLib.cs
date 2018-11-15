1038bf16e2cefe2f5938effdc52fe9d35c847b87
Jalindar <jalindarpandharkar@gmail.com>
2018-11-15 13:41:08 +0530

    Solution Created
    Solution Created

---
+++ CalculatorHelper/CalculatorHelper/CalculatorLib.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorHelper
+{
+    public class CalculatorLib
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (b != 0)
+                return a / b;
+            else
+                return 0;
+        }
+    }
+}
---
+++ CalculatorHelper/CalculatorHelperTests/CalculatorHelperTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorHelper;
+
+namespace CalculatorHelperTests
+{
+    [TestClass]
+    public class CalculatorHelperTest
+    {
+        [TestMethod]
+        public void Add_HappyPath()
+        {
+            Assert.AreEqual(20, CalculatorLib.Sum(15, 5));
+        }
+
+        [TestMethod]
+        public void Sum_HappyPath()
+        {
+            Assert.AreEqual(10, CalculatorLib.Substract(15, 5));
+        }
+
+        [TestMethod]
+        public void Multiplication_HappyPath()
+        {
+            Assert.AreEqual(75, CalculatorLib.Multiplication(15, 5));
+
+        }
+
+        [TestMethod]
+        public void Substract_HappyPath()
+        {
+            Assert.AreEqual(3, CalculatorLib.Division(15, 5));
+        }
+
+        [TestMethod]
+        public void Substract_DivideZero()
+        {
+            Assert.AreEqual(0, CalculatorLib.Division(15, 0));
+        }
+    }
+}
